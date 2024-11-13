f95634b87d54cecb5201b6b00bc14438f45fa1b4
Loic Maria <lmaria@z2r9p8.42lyon.fr>
2024-11-13 13:51:26 +0100

    First commit

---
+++ TDD/test_ft_str_something.c
@@ -0,0 +1,10 @@
+#include "../libft.h"
+#include <assert.h>
+#include <stdio.h>
+
+
+void	test_ft_strlen(void)
+{
+	assert(ft_strlen() == 1);
+	printf("Tous les tests pour ft_strlen sont passes \n");
+}
---
+++ libft.h
@@ -0,0 +1,17 @@
+#ifndef LIBFT_H
+# define LIBFT_H
+
+int		ft_isalnum(int c);
+int		ft_isalpha(int c);
+int		ft_isascii(int c);
+int		ft_isdigit(int c);
+int		ft_isprint(int c);
+size_t	ft_strlen(const char *s);
+size_t	ft_strlcpy(char *dst, const char *src, size_t size);
+size_t	ft_strlcat(char *dst, const char *src, size_t size);
+char	*strchr(const char *s, int c);
+char	*strrchr(const char *s, int c);
+int		strncmp(const char *s1, const char *s2, size_t n);
+char	*strnstr(const char *big, const char *little, size_t len);
+
+#endif
