e034e96b3249f72d97bf4eacf823d59b0bc96c81
Loic Maria <lmaria@z2r11p7.42lyon.fr>
2024-11-14 19:10:09 +0100

    add 42 Header

--- TDD/test_ft_str_something.c
+++ TDD/test_ft_str_something.c
@@ -1,7 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   test_ft_str_something.c                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lmaria <lmaria@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/14 19:05:02 by lmaria            #+#    #+#             */
+/*   Updated: 2024/11/14 19:05:15 by lmaria           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../libft.h"
 #include <assert.h>
 #include <stdio.h>
-#include <string.h>
+
+//#include <string.h>
 
 void	test_ft_strlen(void)
 {
@@ -23,78 +36,72 @@ void	test_ft_strlen(void)
 	printf("All tests for ft_strlen passed\n");
 }
 
-void	test_ft_strlcpy(void)
+// void	test_ft_strlcpy(void)
+// {
+// 	char	dest[20];
+// 	size_t	len;
+
+// 	// Test de base : chaîne normale et buffer suffisant
+// 	len = ft_strlcpy(dest, "Hello, World!", sizeof(dest));
+// 	assert(strcmp(dest, "Hello, World!") == 0);
+// 	// La chaîne devrait être copiée entièrement
+// 	assert(len == 13); // Longueur de "Hello, World!" est 13
+// 	// Test avec un buffer plus petit que la chaîne source
+// 	len = ft_strlcpy(dest, "Hello, World!", 6);
+// 	// Taille 6 pour dest (5 caractères + '\0')
+// 	assert(strcmp(dest, "Hello") == 0);
+// 	// La chaîne devrait être tronquée à "Hello"
+// 	assert(len == 13);
+// 	// La fonction doit retourner la longueur totale de la source
+// 	// Test avec un buffer de taille zéro
+// 	len = ft_strlcpy(dest, "Hello", 0);
+// 	assert(len == 5);
+// 	// La fonction doit retourner la longueur totale de la source
+// 	// Aucune copie ne devrait être faite dans dest,
+// 	// donc pas besoin de tester dest ici
+// 	// Test avec une chaîne vide
+// 	len = ft_strlcpy(dest, "", sizeof(dest));
+// 	assert(strcmp(dest, "") == 0); // La chaîne de destination doit être vide
+// 	assert(len == 0);              // Longueur de la chaîne source est 0
+// 	// Test avec des caractères non imprimables
+// 	len = ft_strlcpy(dest, "Hello\x7FWorld", sizeof(dest));
+// 	// Utilisation du caractère DEL (ASCII 127)
+// 	assert(strcmp(dest, "Hello\x7FWorld") == 0);
+// 	// La chaîne doit être copiée avec le caractère non imprimable
+// 	assert(len == 11);
+// 	// Longueur de "Hello\x7FWorld" est 11
+// 	printf("All tests for ft_strlcpy passed\n");
+// }
+
+void	test_ft_strncmp(void)
 {
-	char	dest[20];
-	size_t	len;
-
-	// Test de base : chaîne normale et buffer suffisant
-	len = ft_strlcpy(dest, "Hello, World!", sizeof(dest));
-	assert(strcmp(dest, "Hello, World!") == 0);
-	// La chaîne devrait être copiée entièrement
-	assert(len == 13); // Longueur de "Hello, World!" est 13
-	// Test avec un buffer plus petit que la chaîne source
-	len = ft_strlcpy(dest, "Hello, World!", 6);
-	// Taille 6 pour dest (5 caractères + '\0')
-	assert(strcmp(dest, "Hello") == 0);
-	// La chaîne devrait être tronquée à "Hello"
-	assert(len == 13);
-	// La fonction doit retourner la longueur totale de la source
-	// Test avec un buffer de taille zéro
-	len = ft_strlcpy(dest, "Hello", 0);
-	assert(len == 5);
-	// La fonction doit retourner la longueur totale de la source
-	// Aucune copie ne devrait être faite dans dest,
-	// donc pas besoin de tester dest ici
-	// Test avec une chaîne vide
-	len = ft_strlcpy(dest, "", sizeof(dest));
-	assert(strcmp(dest, "") == 0); // La chaîne de destination doit être vide
-	assert(len == 0);              // Longueur de la chaîne source est 0
-	// Test avec des caractères non imprimables
-	len = ft_strlcpy(dest, "Hello\x7FWorld", sizeof(dest));
-	// Utilisation du caractère DEL (ASCII 127)
-	assert(strcmp(dest, "Hello\x7FWorld") == 0);
-	// La chaîne doit être copiée avec le caractère non imprimable
-	assert(len == 11);
-	// Longueur de "Hello\x7FWorld" est 11
-	printf("All tests for ft_strlcpy passed\n");
-}
-
-void test_ft_strncmp(void)
-{
-    // Test identical strings
-    assert(ft_strncmp("Hello", "Hello", 5) == 0);
-    assert(ft_strncmp("Hello", "Hello", 0) == 0);
-
-    // Test different strings
-    assert(ft_strncmp("Hello", "World", 5) == -15);
-    assert(ft_strncmp("Hello", "Hella", 5) == 14);
-
-    // Test prefix strings
-    assert(ft_strncmp("Hello", "Hell", 4) == 0);
-    assert(ft_strncmp("Hello", "Hell", 5) == 111);
-
-    // Test different lengths
-    assert(ft_strncmp("Hello", "HelloWorld", 5) == 0);
-    assert(ft_strncmp("Hello", "HelloWorld", 10) == -87);
-
-    // Test special characters
-    assert(ft_strncmp("Hello!", "Hello!", 6) == 0);
-    assert(ft_strncmp("Hello!", "Hello?", 6) == -1);
-
-    // Test length of 0
-    assert(ft_strncmp("Hello", "World", 0) == 0);
-
-    // Test non-printable characters
-    assert(ft_strncmp("Hello\x01", "Hello\x01", 6) == 0);
-    assert(ft_strncmp("Hello\x01", "Hello\x02", 6) == -1);
-
-    printf("All tests for ft_strncmp passed!\n");
+	// Test identical strings
+	assert(ft_strncmp("Hello", "Hello", 5) == 0);
+	assert(ft_strncmp("Hello", "Hello", 0) == 0);
+	// Test different strings
+	assert(ft_strncmp("Hello", "World", 5) == -15);
+	assert(ft_strncmp("Hello", "Hella", 5) == 14);
+	// Test prefix strings
+	assert(ft_strncmp("Hello", "Hell", 4) == 0);
+	assert(ft_strncmp("Hello", "Hell", 5) == 111);
+	// Test different lengths
+	assert(ft_strncmp("Hello", "HelloWorld", 5) == 0);
+	assert(ft_strncmp("Hello", "HelloWorld", 10) == -87);
+	// Test special characters
+	assert(ft_strncmp("Hello!", "Hello!", 6) == 0);
+	assert(ft_strncmp("Hello!", "Hello?", 6) == -1);
+	// Test length of 0
+	assert(ft_strncmp("Hello", "World", 0) == 0);
+	// Test non-printable characters
+	assert(ft_strncmp("Hello\x01", "Hello\x01", 6) == 0);
+	assert(ft_strncmp("Hello\x01", "Hello\x02", 6) == -1);
+	printf("All tests for ft_strncmp passed!\n");
 }
 
 int	main(void)
 {
 	test_ft_strlen();
-	test_ft_strlcpy();
+	// test_ft_strlcpy();
+	test_ft_strncmp();
 	return (0);
 }
--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -1,8 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lmaria <lmaria@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/14 19:00:02 by lmaria            #+#    #+#             */
+/*   Updated: 2024/11/14 19:04:03 by lmaria           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "libft.h"
 
 size_t	ft_strlcpy(char *dst, const char *src, size_t size)
 {
-	unsigned int i;
+	unsigned int	i;
 
 	i = 0;
 	while (i < (size - 1) && src[i])
